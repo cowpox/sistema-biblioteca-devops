8c6e56910c4ec215947d3f91aeabe3e5be22d650
Adriano Lucio Uchoa Brandao <adrianolucio@gmail.com>
2026-06-06 10:13:16 -0300

    Cria estrutura inicial Spring Boot

---
+++ src/test/java/br/uel/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,15 @@
+package br.uel.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
